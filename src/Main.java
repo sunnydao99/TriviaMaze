9fd15dacc2dad3a3adf3497f751ab0813cb8f113
annguyen <anguyen.rain@gmail.com>
2022-10-21 11:12:31 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Model.Room1Model;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello");
+        Room1Model r1Model = new Room1Model();
+        r1Model.connect();
+    }
+}

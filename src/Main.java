0ef121bfcf93a3e8adcc605ecf623619f9fea16d
An Nguyen <anguyen.rain@gmail.com>
2022-11-03 23:02:46 -0700

    Created new Folder Database and update Inventory and QA class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import Model.Inventory;
+
 import Model.QA;
 
 import java.io.FileNotFoundException;
@@ -8,14 +8,14 @@ public class Main {
     public static void main(String[] args) {
 	// An Testing
 
-        Inventory bankQA = new QA();
+        QA bankQA = new QA();
         bankQA.createDB("Database_QA.db");
         bankQA.createTableMC();
         bankQA.createTableTF();
         bankQA.createTableSA();
 
         bankQA.connection();
-        /*bankQA.insertTableMC();// completed tableMC
+       /* bankQA.insertTableMC();// completed tableMC
         bankQA.insertTableSA(); //completed tableSA
         bankQA.insertTableTF();*/
 

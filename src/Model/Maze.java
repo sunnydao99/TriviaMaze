1e22cb0462cb63ff8fdbe49677ddf8b674ddfdef
Sunnydao99 <sunnydao1999@gmail.com>
2022-12-15 10:15:54 -0800

    Adding JavaDocs

--- src/Model/Maze.java
+++ src/Model/Maze.java
@@ -3,9 +3,16 @@ package Model;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * This class is created to generate the maze
+ * @version 12.14.22 (latest update)
+ * @author An Nguyee=n , Satinder Singh
+ */
 public class Maze implements Serializable {
     private static final int RECT_SIZE = 60;
-
+    /**
+     * This method is using 2D array to create the maze
+     */
     private static final int[][] MAZE =
             {       {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                     {1, 5, 1, 0, 4, 0, 0, 0, 0, 0, 4, 0, 1},
@@ -19,10 +26,17 @@ public class Maze implements Serializable {
                     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
             };
 
-
+    /**
+     * This method is used to return the size of rectangle
+     * @return int the value of size
+     */
     public static int myRectSize() {
         return RECT_SIZE;
     }
+    /**
+     * This method is created to get the maze
+     * @return int the maze
+     */
     public static int[][] getMAZE() {
         return MAZE;
     }
